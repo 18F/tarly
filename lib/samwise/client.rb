99a976e9efff0336d36bca5522cc8bf2925832df
Jacob Harris <jacob.harris@gsa.gov>
2016-01-04 23:20:44 -0500

    Tests only run if you had the API key ENV variable
    Also changed to remove circular references in named variables

--- lib/samwise/client.rb
+++ lib/samwise/client.rb
@@ -3,23 +3,23 @@ require 'json'
 
 module Samwise
   class Client
-    def initialize(api_key: api_key)
+    def initialize(api_key:)
       @api_key = api_key || ENV['DATA_DOT_GOV_API_KEY']
     end
 
-    def get_duns_info(duns: nil)
+    def get_duns_info(duns:)
       response = lookup_duns(duns: duns)
       JSON.parse(response.body)
     end
 
-    def duns_is_valid?(duns: nil)
+    def duns_is_valid?(duns:)
       response = lookup_duns(duns: duns)
       response.status == 200
     end
 
     private
 
-    def lookup_duns(duns: duns)
+    def lookup_duns(duns:)
       duns = Samwise::Util.format_duns(duns: duns)
       response = Faraday.get(Samwise::Protocol.duns_url(duns: duns, api_key: @api_key))
     end
